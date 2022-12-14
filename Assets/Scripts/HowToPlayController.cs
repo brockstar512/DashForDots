bed0ac5be776a462ed6db7aa1b949421250ebd0d
brockstar512 <60516979+brockstar512@users.noreply.github.com>
2022-12-14 10:48:45 -0600

    added git ignore

---
+++ Assets/Scripts/HowToPlayController.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class HowToPlayController : MonoBehaviour
+{
+    [SerializeField] Transform paginationUI;
+    [SerializeField] Transform pageParent;
+    [SerializeField] Button pageController;
+    [SerializeField]  LineRenderer lr;
+    CanvasGroup currentCG;
+    int index = 0;
+    List<Transform> dots;
+
+    private void Awake()
+    {
+        pageController.onClick.RemoveAllListeners();
+        pageController.onClick.AddListener(Next);
+        lr = GetComponent<LineRenderer>();
+        
+    }
+    private void OnEnable()
+    {
+        Debug.Log("Enable");
+        index = 0;
+        HandleDot();
+        for (int i = 0; i < pageParent.childCount; i++)
+        {
+            if(i == 0)
+            {
+                pageParent.GetChild(i).GetComponent<CanvasGroup>().alpha = 1;
+                //Transform dot = paginationUI.GetChild(i).GetChild(0);
+                //dot.DOScale(1, .25f).SetEase(Ease.OutBounce);
+
+                continue;
+            }
+            pageParent.GetChild(i).GetComponent<CanvasGroup>().alpha = 0;
+        }
+    }
+    void Next()
+    {
+        ManagePages();
+    }
+
+    void ManagePages()
+    {
+        Transform item = pageParent.GetChild(index);
+        currentCG = item.GetComponent<CanvasGroup>();
+        currentCG.DOFade(0, .25f);
+        //Debug.Log(currentCG.gameObject.name);
+        index = Wrap();
+        //Debug.Log(index);
+        item = pageParent.GetChild(index);
+        currentCG = item.GetComponent<CanvasGroup>();
+        //Debug.Log(currentCG.gameObject.name);
+        currentCG.DOFade(1, .25f).SetEase(Ease.InSine);
+        HandleDot();
+
+
+
+    }
+    void HandleDot()
+    {
+        Transform dot = paginationUI.GetChild(index).GetChild(0);
+        dot.DOScale(1, .15f).SetEase(Ease.InSine);
+
+        for(int i = paginationUI.childCount -1; i > index; i--)
+        {
+            dot = paginationUI.GetChild(i).GetChild(0);
+            dot.DOScale(0, .15f).SetEase(Ease.InSine);
+        }
+        DrawLine();
+    }
+    void DrawLine()
+    {
+        lr.positionCount = index + 1;
+        for(int i =0; i< lr.positionCount; i++)
+        {
+            lr.SetPosition(i, paginationUI.GetChild(i).GetChild(0).position);
+        }
+    }
+
+
+    int Wrap()
+    {
+        if (index + 1 >= pageParent.childCount)
+        {
+            index = 0;
+        }
+        else
+        {
+            index++;
+        }
+        return index;
+    }
+
+}
