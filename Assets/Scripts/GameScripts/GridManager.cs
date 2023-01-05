53f02a608591c5a05f4947d68792df9400f101df
brockstar512 <60516979+brockstar512@users.noreply.github.com>
2023-01-05 14:33:16 -0600

    image lines kind of works but rely on a context menu

--- Assets/Scripts/GameScripts/GridManager.cs
+++ Assets/Scripts/GameScripts/GridManager.cs
@@ -15,15 +15,40 @@ public class GridManager : MonoBehaviour
     public Dot neighborDot { get; private set; }
     public Color32 playerColor;
     public Color32 playerOptions;
-    public Transform dot1;
-    public Transform dot2;
-    public LineRenderer lr;
+    public RectTransform dot1;
+    public RectTransform dot2;
+    public Transform worldCanvas;
+
 
 
     [ContextMenu("Add line")]
     public void AddLine()
     {
+        CreateDotConnection(dot1.anchoredPosition, dot2.anchoredPosition);
+    }
 
+    private void CreateDotConnection(Vector2 dotPositionA, Vector2 dotPositionB)
+    {
+        GameObject gameObject = new GameObject("dotConnection", typeof(Image));
+        gameObject.transform.SetParent(worldCanvas, true);
+        gameObject.GetComponent<Image>().color = this.playerColor;
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        Vector2 dir = (dotPositionB - dotPositionA).normalized;
+        float distance = Vector2.Distance(dotPositionA, dotPositionB);
+        rectTransform.anchorMin = new Vector2(.5f, .5f);
+        rectTransform.anchorMax = new Vector2(.5f, .5f);
+        rectTransform.sizeDelta = new Vector2(distance, .25f);
+        rectTransform.anchoredPosition = dotPositionA + dir * distance * .5f;
+        rectTransform.localPosition = new Vector3(.75f, .75f, 0) * dir;
+        rectTransform.localEulerAngles = new Vector3(0, 0, GetAngleFromVectorFloat(dir));
+    }
+    public float GetAngleFromVectorFloat(Vector3 dir)
+    {
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+
+        return n;
     }
 
     public void Init(Transform dotParent, StateManager StateManager)
